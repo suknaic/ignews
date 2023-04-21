a8c36a8a0b2f59de7fef74eefe580663c7d643ac
Felipe Suknaic <fsuknaic@hotmail.com>
2023-04-21 16:12:27 -0500

    button de subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import styles from './home.module.scss'
 import Head from 'next/head'
+import { SubscribeButton } from '@/components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -17,6 +18,8 @@ export default function Home() {
             Get access to all publications <br />
             <span>For $9.90 month</span>
           </p>
+
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
