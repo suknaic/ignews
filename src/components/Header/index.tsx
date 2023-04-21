2f2b11480b8a2d9da1907131dbfbab719a0cb801
Felipe Suknaic <fsuknaic@hotmail.com>
2023-04-21 15:26:29 -0500

    comnponent Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="logo ignews" />
+        <nav>
+          <a href="" className={styles.active}>
+            home
+          </a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import { Header } from '@/components/Header'
 import type { AppProps } from 'next/app'
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
