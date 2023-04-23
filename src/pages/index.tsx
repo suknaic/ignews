96b1a4b35cb92330073e34a422a0cd950ebb5c64
Felipe Suknaic <fsuknaic@hotmail.com>
2023-04-23 11:59:29 -0500

    static site generate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import styles from './home.module.scss'
 import Head from 'next/head'
 import { SubscribeButton } from '@/components/SubscribeButton'
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, GetStaticProps } from 'next'
 import { stripe } from '@/services/stripe'
 
 interface HomeProps {
@@ -43,7 +43,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1MzRbnFZ27igKCUJfwHCAvav', {
     expand: ['product'],
   })
@@ -57,5 +57,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 }
